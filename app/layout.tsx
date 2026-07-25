07a0938c529a7b0d726102d88bec06355732ecb3
metaory <metaory@gmail.com>
2026-07-25 08:47:11 +0330

    rc4

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,8 +97,8 @@ export const viewport: Viewport = {
   ],
   width: "device-width",
   initialScale: 1,
-  maximumScale: 5,
-  userScalable: true,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
